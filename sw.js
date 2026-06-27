91b1d0d7f7acc02c0e1c40cad2c1ed772a00e3b6
Nexova-mty <nexovamx@gmail.com>
2026-06-26 19:36:40 -0600

    PWA: instalable como app en iPhone, Android, iPad, tablets y computadoras

---
+++ sw.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = 'nexova-v1';
+var urlsToCache = [
+  './',
+  './index.html',
+  './demo-nexova.mp4',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  // Network first, fallback to cache
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      // Clone and cache successful responses
+      if (response && response.status === 200) {
+        var responseClone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseClone);
+        });
+      }
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
